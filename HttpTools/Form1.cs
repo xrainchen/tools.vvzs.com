9f664dceedea8a6715a869bee60768616b65ef0b
xrainchen <xrainchen@163.com>
2017-05-03 13:05:28 +0800

    模拟请求

---
+++ HttpTools/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HttpTools
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var html = RPoney.Utilty.Http.RequestHelper.HttpGet(textBox1.Text, Encoding.GetEncoding(textBox3.Text));
+            textBox2.Text = html;
+        }
+    }
+}
